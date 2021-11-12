c7af74eacd25cefe057b25bdbaf6f56909e9ba09
Bob F <bobfdev@gmail.com>
2021-11-12 14:20:47 -0600

    Updates

---
+++ precache-manifest.a319581e38d49a94b6eac730b3b4cff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1dda07df6d6d343936308b794857870",
+    "url": "/react-hooks/index.html"
+  },
+  {
+    "revision": "cfdebd528b953077fe54",
+    "url": "/react-hooks/static/css/main.c69bebdf.chunk.css"
+  },
+  {
+    "revision": "49247e4ebbc3c241889e",
+    "url": "/react-hooks/static/js/2.88912062.chunk.js"
+  },
+  {
+    "revision": "cfdebd528b953077fe54",
+    "url": "/react-hooks/static/js/main.b1b4aed0.chunk.js"
+  },
+  {
+    "revision": "bb9cb6b5f5410997d1d8",
+    "url": "/react-hooks/static/js/runtime~main.d8882185.js"
+  }
+]);
